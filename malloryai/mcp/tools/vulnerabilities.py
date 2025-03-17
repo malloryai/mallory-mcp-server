b10d20867dd246c3b244bb026ead466d96ff7599
Kreshnik Hasanaj <kreshnik.hasanaj@gmail.com>
2025-03-17 17:49:32 +0100

    feat(vulnerability): return Dict instead of coroutine

--- malloryai/mcp/tools/vulnerabilities.py
+++ malloryai/mcp/tools/vulnerabilities.py
@@ -1,4 +1,4 @@
-from typing import Any, Coroutine
+from typing import Any, Dict
 
 from ..server.server import mcp, malloryai_client
 
@@ -6,9 +6,10 @@ from ..server.server import mcp, malloryai_client
 @mcp.tool()
 async def find_vulnerability(
     cve: str = None,
-) -> Coroutine[Any, Any, dict[str, Any]]:
+) -> Dict[str, Any]:
     """Find a vulnerability by CVE
     Args:
         cve (str): The CVE to search for
     """
-    return malloryai_client.vulnerabilities.get_vulnerability(cve)
+    vulnerability = await malloryai_client.vulnerabilities.get_vulnerability(cve)
+    return vulnerability
